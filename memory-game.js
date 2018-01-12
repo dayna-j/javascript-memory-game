7ea123de3867b968f6106bb401f39a5742baeaf0
Dayna Blackwell <daynajblackwell@gmail.com>
2018-01-12 10:46:28 -0700

    Added array of values to hold the numbers displayed in the game tiles.
    Added valueSet array property, randomizedSet array property, randomizeValueSet function, assignValue function, makeNewBoard function.
    
    Added event handler for clicking on game tiles.

--- memory-game.js
+++ memory-game.js
@@ -2,41 +2,66 @@ $(document).ready(function()
 {
 	var gameObject = 
 	{
-		// Properties of the game object.
+		// Properties of the game object
 		_isGameOver: false,
 		_numTurns: 0,
-
+		// Methods of the game object
 		resetGame: function()
 		{
 			return null;
 		},
 		
-		gameBoard: 
+		gameBoard: // game board object
 		{
+			// Properties of the game board object
+
 			tiles: $(".board-square"),
-			randomNumber: function()
+			valueSet: [1,1,2,2,3,3,4,4,5,5,6,6,
+					   7,7,8,8,9,9,10,10,11,11,12,12],
+			randomizedSet: [],
+			// Methods of the game board object
+			
+			randomizeValueSet: function(array)
 			{
-				return Math.floor((Math.random() * 12) + 1);
+				var currentIndex = array.length, temporaryValue, randomIndex;
+				// While there remain elements to shuffle...
+  				while (0 !== currentIndex) 
+				{
+    				// Pick a remaining element...
+    				randomIndex = Math.floor(Math.random() * currentIndex);
+    				currentIndex -= 1;
+
+					// And swap it with the current element.
+    				temporaryValue = array[currentIndex];
+    				array[currentIndex] = array[randomIndex];
+    				array[randomIndex] = temporaryValue;
+  				}
+  				return array;
+			},
+			
+			assignValue: function()
+			{
+				for (var currentTile in tiles)
+				{// iterates over the randomized value set
+					console.log(tiles[currentTile]);
+				}
+			},
+			
+			makeNewBoard: function()
+			{// 1)  Randomize the value set and assign to randomizedSet array property of the gameBoard obj.
+				gameObject.gameBoard.randomizedSet = gameObject.gameBoard.randomizeValueSet(gameObject.gameBoard.valueSet);
+				gameObject.gameBoard.assignValue();
+			//	2) assign randomized values to game tiles
 			}
+			
 		}
 	};
 
-//	alert(gameObject.gameBoard.randomNumber());
-	console.log(gameObject.gameBoard.tiles[0]);
-	console.log(gameObject.resetGame());
-	
 	$(".board-square").on("click", function(event)
 	{
-		console.log(event.target.id);
-		
-		
+		console.log(event.target);
 	});
 	
-	
-	
-	
-	
-	
-	
-	
+	console.log(gameObject.gameBoard.tiles);
+
 });
