22e381022a3c2e8de7b5538a4ee384a24173c20f
Dayna Blackwell <daynajblackwell@gmail.com>
2018-01-12 14:12:16 -0700

    Introduced JQuery each method into assignValues method of the gameBoard Object.
    $.each() will be used to assign numbers from randomizedSet to individual game board tiles.

--- memory-game.js
+++ memory-game.js
@@ -13,15 +13,16 @@ $(document).ready(function()
 		
 		gameBoard: // game board object
 		{
-			// Properties of the game board object
+			// PROPERTIES OF THE GAME BOARD OBJECT
 
-			tiles: $(".board-square"),
+			tiles: $(".board-square"),// tiles contains what is returned by the jquery selector .board-square
 			valueSet: [1,1,2,2,3,3,4,4,5,5,6,6,
 					   7,7,8,8,9,9,10,10,11,11,12,12],
 			randomizedSet: [],
-			// Methods of the game board object
 			
-			randomizeValueSet: function(array)
+			// METHODS OF THE GAME BOARD OBJECT
+			
+			randomizeValueSet: function(array)// shuffle function for value set
 			{
 				var currentIndex = array.length, temporaryValue, randomIndex;
 				// While there remain elements to shuffle...
@@ -39,29 +40,55 @@ $(document).ready(function()
   				return array;
 			},
 			
-			assignValue: function()
+			assignValues: function()// !!!!! WORKING HERE !!!!!
 			{
-				for (var currentTile in tiles)
-				{// iterates over the randomized value set
-					console.log(tiles[currentTile]);
-				}
+//				for (var currentTile in gameObject.gameBoard.tiles)
+//				{// iterates over the randomized value set
+//					console.log(gameObject.gameBoard.tiles[currentTile]);
+//				}
+				
+				// use jquery each method to access each game tile and assign a value
+				// from randomizedSet
+				
+//				gameObject.gameBoard.tiles.each(function()
+//				{
+//					$('this').innerHTML = 'y';
+//				});
+				
+//				$.each(gameObject.gameBoard.tiles, function(){
+//					
+//				});
+				
+				
+				$(".board-square").each(function(){
+					currentTile = $(this);
+					currentTile.innerHTML = gameObject.gameBoard.randomizedSet.pop();
+					console.log(currentTile);
+					
+					
+				});
+				
+				
+				
 			},
 			
-			makeNewBoard: function()
+			makeNewBoard: function()//combines many smaller functions to make the board ready for play
 			{// 1)  Randomize the value set and assign to randomizedSet array property of the gameBoard obj.
 				gameObject.gameBoard.randomizedSet = gameObject.gameBoard.randomizeValueSet(gameObject.gameBoard.valueSet);
-				gameObject.gameBoard.assignValue();
+				gameObject.gameBoard.assignValues();
 			//	2) assign randomized values to game tiles
 			}
-			
 		}
 	};
 
 	$(".board-square").on("click", function(event)
 	{
 		console.log(event.target);
+		event.target.innerHTML = "y";
 	});
 	
 	console.log(gameObject.gameBoard.tiles);
+//	console.log(gameObject.gameBoard.tiles[0]);
+	gameObject.gameBoard.makeNewBoard();
 
 });
